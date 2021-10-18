8fa7bc9a5425af6f7ef77d2bcb574167262d2a1b
David Huillca <david.ffdbhjf@gmail.com>
2021-10-17 20:53:31 -0500

    Se agrego el archivo conexionDAO

---
+++ E_Commerce/DAO/conexionDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace E_Commerce.DAO
+{
+    public class conexionDAO
+    {
+        SqlConnection cn = new SqlConnection(
+        ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+        public SqlConnection getcn
+        {
+            get { return cn; }
+        }
+    }
+}
