793d8bc623835d0a3a884256bc17e229e08b421c
David Huillca <david.ffdbhjf@gmail.com>
2021-10-17 21:03:59 -0500

    Se creo el modelo Categoria y se agrego un model en IMAGENES

---
+++ E_Commerce/Imagenes/Img.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce.IMAGENES
+{
+    public class Img
+    {
+        // Esta clase se creo con el objetivo de que aparesca la carpeta IMAGENES en github
+    }
+}
