202d8e2366620d3c070dd548581d89e45df4c0a3
David Huillca <david.ffdbhjf@gmail.com>
2021-12-01 12:13:01 -0500

    Se agrego el la vista Index

--- E_Commerce/DAO/productoDAO.cs
+++ E_Commerce/DAO/productoDAO.cs
@@ -15,59 +15,79 @@ namespace E_Commerce.DAO
         public IEnumerable<Producto> producto()
         {
             List<Producto> temporal = new List<Producto>();
-            SqlConnection cn = new SqlConnection(cadena);
-            SqlCommand cmd = new SqlCommand("sp_listado_producto", cn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cn.Open();
+            cn = new conexionDAO();
+            cn.getcn.Open();
+            SqlCommand cmd = new SqlCommand("sp_listado_producto", cn.getcn);
             SqlDataReader dr = cmd.ExecuteReader();
-            while (dr.Read())
+            try
             {
-                Producto reg = new Producto();
-                reg.id_prod = dr.GetString(0);
-                reg.codigo = dr.GetString(1);
-                reg.id_categ = dr.GetInt32(2);
-                reg.marca = dr.GetString(3);
-                reg.modelo = dr.GetString(4);
-                reg.descripcion = dr.GetString(5);
-                reg.observacion = dr.GetString(6);
-                reg.fec_compra = dr.GetDateTime(7);
-                reg.stock = dr.GetInt32(8);
-                reg.precio = dr.GetDecimal(9);
-                reg.imagen = dr.GetString(10);
-                reg.calidad = dr.GetDecimal(11);
-                reg.estado = dr.GetInt32(12);
-                temporal.Add(reg);
+                cmd.CommandType = CommandType.StoredProcedure;
+                while (dr.Read())
+                {
+                    Producto reg = new Producto();
+                    reg.id_prod = dr.GetString(0);
+                    reg.codigo = dr.GetString(1);
+                    reg.id_categ = dr.GetInt32(2);
+                    reg.marca = dr.GetString(3);
+                    reg.modelo = dr.GetString(4);
+                    reg.descripcion = dr.GetString(5);
+                    reg.observacion = dr.GetString(6);
+                    reg.fec_compra = dr.GetDateTime(7);
+                    reg.stock = dr.GetInt32(8);
+                    reg.precio = dr.GetDecimal(9);
+                    reg.imagen = dr.GetString(10);
+                    reg.calidad = dr.GetDecimal(11);
+                    reg.estado = dr.GetInt32(12);
+                    temporal.Add(reg);
+                }
             }
-            dr.Close(); cn.Close();
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                dr.Close(); cn.getcn.Close();
+            }            
             return temporal;
         }
         public IEnumerable<Producto> producto_calidad()
         {
             List<Producto> temporal = new List<Producto>();
-            SqlConnection cn = new SqlConnection(cadena);
-            SqlCommand cmd = new SqlCommand("sp_listado_producto_calidad", cn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cn.Open();
+            cn = new conexionDAO();
+            cn.getcn.Open();
+            SqlCommand cmd = new SqlCommand("sp_listado_producto_calidad", cn.getcn);
             SqlDataReader dr = cmd.ExecuteReader();
-            while (dr.Read())
+            try
             {
-                Producto reg = new Producto();
-                reg.id_prod = dr.GetString(0);
-                reg.codigo = dr.GetString(1);
-                reg.id_categ = dr.GetInt32(2);
-                reg.marca = dr.GetString(3);
-                reg.modelo = dr.GetString(4);
-                reg.descripcion = dr.GetString(5);
-                reg.observacion = dr.GetString(6);
-                reg.fec_compra = dr.GetDateTime(7);
-                reg.stock = dr.GetInt32(8);
-                reg.precio = dr.GetDecimal(9);
-                reg.imagen = dr.GetString(10);
-                reg.calidad = dr.GetDecimal(11);
-                reg.estado = dr.GetInt32(12);
-                temporal.Add(reg);
+                cmd.CommandType = CommandType.StoredProcedure;
+                while (dr.Read())
+                {
+                    Producto reg = new Producto();
+                    reg.id_prod = dr.GetString(0);
+                    reg.codigo = dr.GetString(1);
+                    reg.id_categ = dr.GetInt32(2);
+                    reg.marca = dr.GetString(3);
+                    reg.modelo = dr.GetString(4);
+                    reg.descripcion = dr.GetString(5);
+                    reg.observacion = dr.GetString(6);
+                    reg.fec_compra = dr.GetDateTime(7);
+                    reg.stock = dr.GetInt32(8);
+                    reg.precio = dr.GetDecimal(9);
+                    reg.imagen = dr.GetString(10);
+                    reg.calidad = dr.GetDecimal(11);
+                    reg.estado = dr.GetInt32(12);
+                    temporal.Add(reg);
+                }
             }
-            dr.Close(); cn.Close();
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                dr.Close(); cn.getcn.Close();
+            }            
             return temporal;
         }
         public Producto buscar_producto(string id)
